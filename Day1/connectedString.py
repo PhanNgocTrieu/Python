c374b489b6a6db6030b7e84416168b791459c497
PhanNgocTrieu <bettertogetheronit@gmail.com>
2022-02-23 04:38:50 -0800

    Day1 exercise 1-2

---
+++ Day1/connectedString.py
@@ -0,0 +1,18 @@
+print("Hello" + " " + "Trieu John")
+print("I am\nfrom Quang Nam Province!")
+
+"""
+    Fixing this error in the below code:
+    |print(Day1 -  String Manipulation")
+    |print("String Concatenation is done with the "+" sign.")
+    |   print('e.g. print("Hello " + "world")')
+    |print(("New lines can be created with a backslash and n."))
+"""
+
+"""
+    Fixing by this below:
+"""
+print("Day1 -  String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n."))
