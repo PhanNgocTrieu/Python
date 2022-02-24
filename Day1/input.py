77962d6329bdc9d58b2842590b746b850d282c59
PhanNgocTrieu <bettertogetheronit@gmail.com>
2022-02-24 03:53:03 -0800

    Updating

---
+++ Day1/input.py
@@ -0,0 +1,9 @@
+# _input --- function for user enter the input.
+# the format: the input is string.
+
+# Enter name and saying hello to @_name
+print("Hello " + input("What is your name?") + "!")
+
+
+# Doing Exercise: print legth of input
+print(len(input("Enter the input: ")))
